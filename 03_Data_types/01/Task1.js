eb7a47bfe6c4a7d8fe68983968334339ef026078
Yury Kokleev <binformail@gmail.com>
2016-06-27 10:08:03 +0300

    Renaming 02 dir and adding 03, in new branch

---
+++ 03_Data_types/01/Task1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by subs on 16.06.22.
+ */
+/*Arrays
+
+ 1. Write a script that allocates array of 20 integers and initializes each element by its index multiplied by 5. Print the obtained array on the console.
+ 2. Write a script that compares two char arrays lexicographically (letter by letter).
+ 3. Write a script that finds the maximal sequence of equal elements in an array.
+
+ **Example:** [2, 1, 1, 2, 3, 3, 2, 2, 2, 1] -> [2, 2, 2].
+
+ 4. Write a script that finds the maximal increasing sequence in an array.
+
+ **Example:** [3, 2, 3, 4, 2, 2, 4] -> [2, 3, 4].
+ */
