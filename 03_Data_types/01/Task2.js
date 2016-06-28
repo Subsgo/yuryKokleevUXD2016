600f3cad31cdc907feb15d074b9574242277b734
Yury Kokleev <binformail@gmail.com>
2016-06-28 17:02:20 +0300

    01/task2

--- 03_Data_types/01/Task2.js
+++ 03_Data_types/01/Task2.js
@@ -2,20 +2,34 @@
  * Created by subs on 16.06.27.
  */
 /*
-    2. Write a script that compares two char arrays lexicographically (letter by letter).
-*/
+ 2. Write a script that compares two char arrays lexicographically (letter by letter).
+ */
+//we asume that lexicographically is : 
+var inputArr1 = ['a', 'b', 'c'], inputArr2 = ['a', 'b', 'C'];
 
-var inputArr1 = ['a', 'b', 'C'], inputArr2 = ['a', 'b', 'd'];
+function compareArrs(a, b) {
+    let l = 0;
+    a = a.join('');
+    b = b.join('');
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    a = Array.from(a);
+    b = Array.from(b);
 
-function compareArrs  (a, b) {
-    
 
-    return ;
-
-
-
+    (a.length <= b.length) ? l = a.length : l = b.length;
+    for (let i = 0; i < l; i++) {
+        console.log('eq' + a[i] > b[i]);
+        if (a[i] != b[i]) {
+            if (a[i] > b[i]) {
+                return ('b is earlier');
+            } else {
+                return ('a is earlier');
+            }
+        }
+    }
+    return 'equal';
 }
 
 
-
-console.log(compareArrs (inputArr1, inputArr2));
+console.log(compareArrs(inputArr1, inputArr2));
