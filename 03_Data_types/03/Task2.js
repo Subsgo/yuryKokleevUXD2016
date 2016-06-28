675c1c361166a598cbdb14bb147c0bb1777f0385
Yury Kokleev <binformail@gmail.com>
2016-06-28 12:38:09 +0300

    03/task2

--- 03_Data_types/03/Task2.js
+++ 03_Data_types/03/Task2.js
@@ -9,13 +9,14 @@
  * Attach it to the array object
  * Read about `prototype` and how to attach methods
 */
-var arr = [1, 2, 1, 4, 1, 3, 4, 1, 111, 3, 2, 1, "1"];
-arr.remove = function(val){
+
+Array.prototype.remove = function(val){
 
     for (let i = 0; i < this.length; i++ ){
         if (this[i] === val) this.splice(i,1);
     }
     return this;
 }
+var arr = [1, 2, 1, 4, 1, 3, 4, 1, 111, 3, 2, 1, "1"];
 console.log(arr.remove(1));
-// attached as a method but also as a property, how to avoid ?
+// now its clear
