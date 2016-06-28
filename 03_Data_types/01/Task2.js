e2585ade15a4f97206180d49397db312938195b2
Yury Kokleev <binformail@gmail.com>
2016-06-28 11:15:47 +0300

    01/Task2

---
+++ 03_Data_types/01/Task2.js
@@ -0,0 +1,21 @@
+/**
+ * Created by subs on 16.06.27.
+ */
+/*
+    2. Write a script that compares two char arrays lexicographically (letter by letter).
+*/
+
+var inputArr1 = ['a', 'b', 'C'], inputArr2 = ['a', 'b', 'd'];
+
+function compareArrs  (a, b) {
+    
+
+    return ;
+
+
+
+}
+
+
+
+console.log(compareArrs (inputArr1, inputArr2));
