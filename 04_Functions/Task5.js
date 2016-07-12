a8c5e8f5b10ab0b54eea52914f6f260628352726
Yury Kokleev <binformail@gmail.com>
2016-07-12 10:49:02 +0300

    [04 Functions] 05 Done

---
+++ 04_Functions/Task5.js
@@ -0,0 +1,13 @@
+/**
+ * Created by subs on 16.07.12.
+ */
+
+/*
+ Task 5. IIFE Make the function conc immediately-invoked function expression
+*/
+
+(function (a, b) {
+
+    return console.log(a.toString()+b);
+
+}) (1,1);
